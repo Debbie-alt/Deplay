b0e8bdece5c5e9a6caac6b55fafc4c2c9512780c
Debbie-alt <115513115+Debbie-alt@users.noreply.github.com>
2024-04-10 22:35:24 +0100

    Add shadcnUI components

---
+++ src/app/page2.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+export default function testShadcn (){
+      return(
+            <div className="w-screen bg-green-400 text-center ">
+                  This is my Page testing Router
+                <Button>I love Samuel Ayadi</Button>
+            </div>
+      )
+
+}
